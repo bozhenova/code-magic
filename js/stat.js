584d7f82c153dd7b7d9b678728aec5d5c55cbc99
Margo <margo.bozhenova@gmail.com>
2019-11-08 02:52:54 +0300

    Added renderCloud function and eslint settings

---
+++ js/stat.js
@@ -0,0 +1,31 @@
+'use strict';
+var CLOUD_WIDTH = 500;
+var CLOUD_HEIGHT = 200;
+var CLOUD_X = 100;
+var CLOUD_Y = 50;
+var GAP = 10;
+var FONT_GAP = 15;
+var TEXT_WIDTH = 50;
+var BAR_HEIGT = 20;
+
+var renderCloud = function (ctx, x, y, color) {
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
+
+};
+
+window.renderStatistics = function (ctx) {
+  renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.3)');
+  renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
+
+  ctx.fillStyle = '#000';
+  ctx.fillText('Вы', CLOUD_X + GAP, CLOUD_Y + GAP + FONT_GAP);
+  ctx.fillRect(CLOUD_X + GAP + TEXT_WIDTH, CLOUD_Y + GAP, 430, BAR_HEIGHT);
+
+  ctx.fillText('Иван', 110, 105);
+  ctx.fillRect(160, 90, 430, 20);
+
+  ctx.fillText('Юлия', 110, 135);
+  ctx.fillRect(160, 120, 430, 20);
+
+};

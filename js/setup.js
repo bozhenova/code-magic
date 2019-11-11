6c9a304e6732ce0cf77cd09fd92802c9727f850b
Margo <margo.bozhenova@gmail.com>
2019-11-12 01:46:22 +0300

    Created renderWizard function

--- js/setup.js
+++ js/setup.js
@@ -4,9 +4,7 @@
   var userDialog = document.querySelector('.setup');
   userDialog.classList.remove('hidden');
 
-  document.querySelector('.setup-similar').classList.remove('hidden');
-
-  var similarListElement = document.querySelector('.setup-similar-list');
+  var similarListElement = userDialog.querySelector('.setup-similar-list');
   var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
 
   var wizardNames = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
@@ -32,6 +30,23 @@
   }
 
   wizardsGeneration();
-  console.log(wizards);
+
+  function renderWizard(wizard) {
+    var wizardElement = similarWizardTemplate.cloneNode(true);
+    wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
+    wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
+    wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
+    return wizardElement;
+  }
+
+  var fragment = document.createDocumentFragment();
+  for (var i = 0; i < wizards.length; i++) {
+    fragment.appendChild(renderWizard(wizards[i]));
+  }
+  similarListElement.appendChild(fragment);
+
+  userDialog.querySelector('.setup-similar').classList.remove('hidden');
+
+  console.log(similarListElement);
 
 })();
